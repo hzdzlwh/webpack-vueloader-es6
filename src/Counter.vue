bf71524e4e672c076803c8ac54b2d870eac1214c
whli <whli@leqee.com>
2016-12-14 15:20:50 +0800

    add a component

--- src/Counter.vue
+++ src/Counter.vue
@@ -1,12 +1,20 @@
 <template>
-  <div class="example">{{ msg }}</div>
+  <div class="example">
+    {{ msg }}
+    <comp-a></comp-a>
+  </div>
 </template>
 <script>
+import CompA from './components/a.vue'
+console.log(CompA)
 export default {
   data () {
     return {
       msg: 'hello'
     }
+  },
+  components: {
+    CompA
   }
 }
 </script>
---
+++ src/components/a.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+    <h2 class="red">{{msg}}</h2>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      msg: 'hello from Component A!'
+    }
+  }
+}
+</script>
+<style>
+.container {
+	border: 1px solid #00f;
+}
+.red {
+	color: #f00;
+}
+</style>
